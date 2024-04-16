84dc415284e45342942478cffa7e106803cc3c7b
Alexandr <vin89441@gmail.com>
2024-04-16 15:58:40 +0300

    refactor main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,89 +1,13 @@
 package main
 
 import (
-	"fmt"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/joho/godotenv"
-	"github.com/zeze322/telegram-bot-weather-forecast/internal/weather"
+	"github.com/zeze322/telegram-bot-weather-forecast/internal/tgBot"
 	"log"
-	"os"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
-	if err != nil {
+	if err := tgBot.BotInit(); err != nil {
 		log.Fatal(err)
 	}
-
-	bot.Debug = true
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		w, err := weather.Forecast(update.Message.Text, os.Getenv("WEATHER_TOKEN"))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var text string
-
-		switch w.Condition[0].Description {
-		case "пасмурно":
-			text = fmt.Sprintf(`%s:
-Температура %.f°C, %s  🌥️
-Давление %dмм рт. ст.
-Ветер %.f м/с`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed)
-		case "облачно с прояснениями":
-			text = fmt.Sprintf(`%s:
-Температура %.f°C, %s  ⛅
-Давление %dмм рт. ст.
-Ветер %.f м/с`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed)
-		case "ясно":
-			text = fmt.Sprintf(`%s:
-Температура %.f°C, %s  ☀️
-Давление %dмм рт. ст.
-Ветер %.f м/с`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed)
-		case "плотный туман":
-			text = fmt.Sprintf(`%s:
-Температура %.f°C, %s  🌫️
-Давление %dмм рт. ст.
-Ветер %.f м/с`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed)
-		case "небольшой снег":
-			text = fmt.Sprintf(`%s:
-Температура %.f°C, %s  ❄️
-Давление %dмм рт. ст.️
-Ветер %.f м/с`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed)
-		case "дождь":
-			text = fmt.Sprintf(`%s:
-Температура %.f°C, %s  🌧️
-Давление %dмм рт. ст.
-Ветер %.f м/с`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed)
-		case "переменная облачность":
-			text = fmt.Sprintf(`%s:
-Температура %.f°C, %s  ☁️
-Давление %dмм рт. ст.
-Ветер %.f м/с`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed)
-		case "небольшой дождь":
-			text = fmt.Sprintf(`%s:
-Температура %.f°C, %s  🌧️
-Давление %dмм рт. ст.
-Ветер %.f м/с`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed)
-		default:
-			text = fmt.Sprintf(`%s:
-Температура %.f°C, %s
-Давление %dмм рт. ст.
-Ветер %.f м/с`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed)
-		}
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
-		bot.Send(msg)
-	}
+	
 }
---
+++ internal/tgBot/tgBot.go
@@ -0,0 +1,111 @@
+package tgBot
+
+import (
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"github.com/zeze322/telegram-bot-weather-forecast/internal/weather"
+	"os"
+)
+
+func BotInit() error {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return fmt.Errorf("failed to load .env file")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		return fmt.Errorf("failed to init telegram bot")
+	}
+
+	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		w, err := weather.Forecast(update.Message.Text, os.Getenv("WEATHER_TOKEN"))
+		if err != nil {
+			continue
+		}
+
+		var text string
+
+		switch w.Condition[0].Description {
+		case "пасмурно":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  🌥️
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		case "облачно с прояснениями":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  ⛅
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		case "ясно":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  ☀️
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		case "плотный туман":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  🌫️
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		case "небольшой снег":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  🌨️
+Давление: %dмм рт. ст.️
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		case "дождь":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  🌧️
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		case "переменная облачность":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  ☁️
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		case "небольшой дождь":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  🌨️
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		case "небольшая облачность":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  ⛅
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		case "сильный дождь":
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s  🌨️
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		default:
+			text = fmt.Sprintf(`%s
+Температура: %.f°C, %s
+Давление: %dмм рт. ст.
+Ветер: %.f м/с
+Влажность: %d%%`, w.Name, w.Main.Temp, w.Condition[0].Description, w.Main.Pressure, w.Wind.Speed, w.Main.Humidity)
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
+		bot.Send(msg)
+	}
+
+	return nil
+}
